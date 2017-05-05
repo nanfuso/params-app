2a28ecac58e5fd86e7504b69359884e6051ed6d7
natail <nataleanfuso@gmail.com>
2017-05-04 21:25:29 -0500

    added forms

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -5,6 +5,13 @@ class ParamsExamplesController < ApplicationController
 
     def url_segment
         @message = params["this_is_a_key"]
+    end
 
+    def form_show
+
+    end
+
+    def form_send
+        @message = params["form_message"]
     end
 end
